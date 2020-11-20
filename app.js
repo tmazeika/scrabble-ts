c5e68d12d649f2e7c5693eb09f9f184e919fb40d
tmazeika <tj@mazeika.me>
2020-11-19 19:46:35 -0500

    Simplify structure

--- app.js
+++ app.js
@@ -1,41 +1,184 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import { dictionary } from './dictionary.js';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const rng = new Math.seedrandom('hello.');
 
-var app = express();
+const ACROSS = 'across';
+const DOWN = 'down';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const points = {
+  a: 1,
+  b: 3,
+  c: 3,
+  d: 2,
+  e: 1,
+  f: 4,
+  g: 2,
+  h: 4,
+  i: 1,
+  j: 8,
+  k: 5,
+  l: 1,
+  m: 3,
+  n: 1,
+  o: 1,
+  p: 3,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 1,
+  v: 4,
+  w: 4,
+  x: 8,
+  y: 4,
+  z: 10,
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+class Scrabble {
+  constructor(...players) {
+    this.bag = {
+      a: 9,
+      b: 2,
+      c: 2,
+      d: 4,
+      e: 12,
+      f: 2,
+      g: 3,
+      h: 2,
+      i: 9,
+      j: 1,
+      k: 1,
+      l: 4,
+      m: 2,
+      n: 6,
+      o: 8,
+      p: 2,
+      q: 1,
+      r: 6,
+      s: 4,
+      t: 6,
+      u: 4,
+      v: 2,
+      w: 2,
+      x: 1,
+      y: 2,
+      z: 1,
+      _: 2,
+    };
+    this.turn = false;
+    this.players = players;
+    this.board = new Array(15);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = new Array(15);
+    }
+    this.board[7][7] = { val: '+', };
+    players.forEach(this.dealTiles.bind(this));
+  }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  getAllMoves(player) {
+  }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  dealTiles(player) {
+    let n = 7 - player.rack.length;
+    const bag = this.bagToArray();
+    for (; n > 0; n--) {
+      if (bag.length === 0) {
+        return false;
+      }
+      const letter = bag[Math.floor(rng.quick() * bag.length)];
+      this.bag[letter]--;
+      player.rack.push(letter);
+    }
+    return true;
+  }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  bagToArray() {
+    return Object.entries(this.bag).reduce((acc, [letter, count]) => {
+      for (; count > 0; count--) {
+        acc.push(letter);
+      }
+      return acc;
+    }, []);
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  toString() {
+    let str = '  '
+    for (let i = 0; i < this.board.length; i++) {
+      str += i.toString(16) + ' ';
+    }
+    str += '\n';
+    for (let i = 0; i < this.board.length; i++) {
+      const row = this.board[i];
+      str += i.toString(16) + ' ';
+      for (const tile of row) {
+        str += (tile ? tile.val : '-') + ' ';
+      }
+      str += '\n';
+    }
+    return str;
+  }
+}
 
-module.exports = app;
+class Player {
+  constructor(name, turn) {
+    this.name = name;
+    this.turn = turn;
+    this.rack = [];
+    this.points = 0;
+  }
+
+  play(game, word, row, col, dir) {
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i];
+
+      if (game.board[row][col]?.val !== letter) {
+        this.rack.splice(this.rack.indexOf(letter), 1);
+        game.board[row][col] = { val: letter };
+      }
+      this.points += points[letter];
+
+      switch (dir) {
+        case DOWN:
+          row++;
+          break;
+        case ACROSS:
+          col++;
+          break;
+      }
+    }
+    game.dealTiles(this);
+    game.turn = !game.turn;
+  }
+
+  toString() {
+    let str = 'Name: ' + this.name + '\n';
+    str += 'Points: ' + this.points + '\n';
+    str += 'Rack: ' + this.rack.join(', ');
+    return str;
+  }
+}
+
+const player1 = new Player('TJ', false);
+const player2 = new Player('Justine', true);
+const game = new Scrabble(player1, player2);
+
+console.log(game.toString());
+console.log(player1.toString());
+console.log(player2.toString());
+
+window.play1 = function(row, col, dir, word) {
+  play(player1, row, col, dir, word);
+}
+
+window.play2 = function(row, col, dir, word) {
+  play(player2, row, col, dir, word);
+}
+
+function play(player, row, col, dir, word) {
+  player.play(game, word, row, col, dir);
+  console.log(game.toString());
+  console.log(player1.toString());
+  console.log(player2.toString());
+}
+
+window.dictionary = dictionary;
